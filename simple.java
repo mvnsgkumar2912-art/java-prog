5e5a474ec9a990c725704cf32a5cd9481b7804cb
Gnaneswar <mvnsgkumar2912@gmail.com>
2026-08-12 10:34:17 +0530

    java-code

---
+++ simple.java
@@ -0,0 +1,24 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        // Variables to hold numbers
+        int num1 = 15;
+        int num2 = 5;
+
+        // Performing basic math calculations
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+
+        // Displaying results in the console
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("---------------------");
+        System.out.println("Addition (+):       " + sum);
+        System.out.println("Subtraction (-):    " + difference);
+        System.out.println("Multiplication (*): " + product);
+        System.out.println("Division (/):       " + quotient);
+        System.out.println("Remainder (%):      " + remainder);
+    }
+}
